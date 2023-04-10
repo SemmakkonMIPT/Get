35283e3dab9178fa167867384a2a15d2bc4b782c
Konvisar Semen <konvisar.sm@phystech.edu>
2023-04-10 11:01:01 +0300

    remode code

--- GPIO/5-1-adc-simple.py
+++ GPIO/5-1-adc-simple.py
@@ -1,32 +1,38 @@
 import RPi.GPIO as gpio
 import sys
 from time import sleep
+
 gpio.setmode(gpio.BCM)
-dac=[26, 19, 13, 6, 5, 11, 9, 10]
-comp=4
-troyka=17
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+comp = 4
+troyka = 17
 gpio.setup(dac, gpio.OUT)
-gpio.setup(troyka,gpio.OUT, initial=gpio.HIGH)
+gpio.setup(troyka, gpio.OUT, initial=gpio.HIGH)
 gpio.setup(comp, gpio.IN)
 
-def perev(a):
-    return [int (elem) for elem in bin(a)[2:].zfill(8)]
+
+def ToBin(a):
+    return [int(elem) for elem in bin(a)[2:].zfill(8)]
+
 
 def adc():
     for i in range(256):
-        daccc=perev(i)
-        gpio.output(dac, daccc)
-        compvalue=gpio.input(comp)
+        out = ToBin(i)
+        gpio.output(dac, out)
         sleep(0.05)
-        if compvalue==0:
+        inp = gpio.input(comp)
+
+        if inp == 0:
             return i
+    return 0
+
 
 try:
     while True:
-        i=adc()
+        i = adc()
         if i!=0:
             print(i, '{:.2f}v'.format(3.3*i/256))
-        
+
 finally:
     gpio.output(dac, 0)
-    gpio.cleanup()   
+    gpio.cleanup()
