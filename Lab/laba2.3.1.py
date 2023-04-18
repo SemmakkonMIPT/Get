a959347164f1b993986c04b666d4b87ef95cadac
Konvisar Semen <konvisar.sm@phystech.edu>
2023-04-18 20:07:38 +0300

    add some data laba2.3.1

--- Lab/laba2.3.1.py
+++ Lab/laba2.3.1.py
@@ -52,8 +52,25 @@ Ppr2 = np.array([Pprv for i in P2])
 P_P2 = P2-Ppr2*10**4
 lnP_P2 = np.log(P_P2)
 
-plt.scatter(t1, lnP_P1)
-plt.scatter(t2, lnP_P2)
+P1u = np.array([77, 81, 88, 97, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610])
+P1n = np.concatenate([P1u, [10*i for i in range(41, 61)]])
+t1u = [i for i in range(len(P1u))]
+t1u = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 33, 34, 35, 36, 38, 39, 40, 42, 43, 44, 45, 47, 48, 49, 50, 52, 53, 54, 55, 57, 58, 59, 61, 63, 64]
+t1un = t1u + [i for i in range(t1u[-1]+1, t1u[-1]+1+len(P1u)-len(t1u))]
+print(t1un)
+#print([i for i in range(t1u[-1]+2)])
+
+for i in range(40, len(P1u)):
+    print(P1u[i], t1un[i], sep = ' ', end = ', ')
+
+
+n = [3]
+if 1 in n:
+    plt.scatter(t1, lnP_P1)
+if 2 in n:
+    plt.scatter(t2, lnP_P2)
+if 3 in n:
+    plt.scatter(t1u, P1u)
 
 plt.show()
 
